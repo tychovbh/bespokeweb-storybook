fcc7792eec02730536386884e5e7c1b95cf96c66
Tycho van Berge Henegouwen <info@bespokeweb.nl>
2021-01-06 23:32:01 +0100

    try to add styles

--- dist/index.js
+++ dist/index.js
@@ -11,3 +11,4 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./Button"), exports);
+__exportStar(require("./button.css"), exports);
