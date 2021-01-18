635b4d5b598ab6dc09f6cdc7f884a34ae5d949f2
Thoompje123 <unconfigured@null.spigotmc.org>
2021-01-18 16:01:01 +0100

    added + converted Modal component to Tailwind

---
+++ src/components/Modals/Modals.js
@@ -0,0 +1,186 @@
+import React, {useEffect} from "react";
+import PropTypes from 'prop-types';
+import * as Texts from '../Texts/Texts'
+
+export const Overlay = ({id, className, appendClassname, children, open}) => {
+    const showClass = open ? 'storybook-modals-visible' : '';
+    return <div
+        id={id}
+        className={`${className ?? 'storybook-modals-overlay'} ${showClass} ${appendClassname}`}
+    >
+        {children}
+    </div>
+};
+Overlay.propTypes = {
+    /**
+     * The id of the Overlay
+     */
+    id: PropTypes.string,
+
+    /**
+     * Use a different classname
+     */
+    className: PropTypes.string,
+
+    /**
+     * Add more classnames
+     */
+    appendClassname: PropTypes.string,
+
+    /**
+     * The title of the Overlay
+     */
+    children: PropTypes.node.isRequired,
+
+    /**
+     * The visibility of the Modal
+     */
+    open: PropTypes.bool,
+};
+Overlay.defaultProps = {
+    appendClassname: '',
+    open: false
+};
+
+export const Modal = ({children, open, onClose}) => {
+    useEffect(() => {
+        if (open) {
+            document.body.classList.add('no-scroll');
+        } else {
+            document.body.classList.remove('no-scroll');
+        }
+    }, [open]);
+
+    return <Overlay open={open} onClose={onClose}>
+        {children}
+        <div className={'storybook-models-onblur'} onClick={onClose}/>
+    </Overlay>
+};
+Modal.propTypes = {
+    /**
+     * The HTML inside the Modal
+     */
+    children: PropTypes.node.isRequired,
+
+    /**
+     * The visibility of the Modal
+     */
+    open: PropTypes.bool,
+
+    /**
+     * The onClose method of the Modal
+     */
+    onClose: PropTypes.func
+};
+Modal.defaultProps = {
+    show: false
+};
+
+export const Container = ({id, className, appendClassname, children}) => {
+    return <div
+        id={id}
+        className={`${className ?? 'storybook-modals-body'} ${appendClassname}`}>
+        {children}
+    </div>
+};
+Container.propTypes = {
+    /**
+     * The id of the Container
+     */
+    id: PropTypes.string,
+
+    /**
+     * Use a different classname
+     */
+    className: PropTypes.string,
+
+    /**
+     * Add more classnames
+     */
+    appendClassname: PropTypes.string,
+
+    /**
+     * The title of the Container
+     */
+    children: PropTypes.node.isRequired
+};
+Container.defaultProps = {
+    appendClassname: ''
+};
+
+export const Body = ({id, className, appendClassname, children}) => {
+    return <div
+        id={id}
+        className={`${className ?? 'storybook-modals-content'} ${appendClassname}`}
+    >
+        {children}
+    </div>
+};
+Body.propTypes = {
+    /**
+     * The id of the Container
+     */
+    id: PropTypes.string,
+
+    /**
+     * Use a different classname
+     */
+    className: PropTypes.string,
+
+    /**
+     * Add more classnames
+     */
+    appendClassname: PropTypes.string,
+
+    /**
+     * The title of the Container
+     */
+    children: PropTypes.node.isRequired
+};
+Body.defaultProps = {
+    appendClassname: ''
+};
+
+export const Footer = ({id, className, appendClassname, children}) => {
+    return <div
+        id={id}
+        className={`${className ?? 'storybook-modals-footer'} ${appendClassname}`}
+    >
+        {children}
+    </div>
+};
+Footer.propTypes = {
+    /**
+     * The id of the Modal
+     */
+    id: PropTypes.string,
+
+    /**
+     * Use a different classname
+     */
+    className: PropTypes.string,
+
+    /**
+     * Add more classnames
+     */
+    appendClassname: PropTypes.string,
+
+    /**
+     * The title of the Button
+     */
+    children: PropTypes.node.isRequired,
+};
+Footer.defaultProps = {
+    appendClassname: '',
+    type: 'default'
+};
+
+export const Title = ({id, className, appendClassname, children}) => {
+    return <Texts.Heading
+        id={id}
+        type={'h3'}
+        appendClassname={`${className ?? 'storybook-modals-title'} ${appendClassname}`}
+    >
+        {children}
+    </Texts.Heading>
+};
---
+++ src/stories/Modals.stories.js
@@ -0,0 +1,50 @@
+import React, {useState} from "react";
+import '../styles/style.css';
+import * as Modals from '../components/Modals/Modals';
+import * as Texts from "../components/Texts/Texts";
+import {Button} from "../components/Buttons/Button";
+
+export default {
+    title: 'Example/Modals',
+    component: Modals.Modal,
+    argsTypes: {}
+};
+
+const Template = (args) => <Modals.Modal {...args}>
+    <Modals.Container>
+        <Modals.Body>
+            <Modals.Title>Deactivate account</Modals.Title>
+            <Texts.Primary>
+                Are you sure you want to deactivate your account? All of your data will be permanently removed. This
+                action cannot be undone.
+            </Texts.Primary>
+        </Modals.Body>
+        <Modals.Footer>
+            <Button type={'default'} onClick={args.onClose}>Close</Button>
+            <Button type={'danger'}>Deactivate</Button>
+        </Modals.Footer>
+    </Modals.Container>
+</Modals.Modal>;
+
+const Template2 = (args) => {
+    const [open, setOpen] = useState(false);
+    return <>
+        <Template {...args} open={open} onClose={() => setOpen(false)}/>
+        <Button type={'primary'} onClick={() => setOpen(true)}>Open model</Button>
+    </>
+};
+
+export const Hidden = Template.bind({});
+Hidden.args = {
+    open: false,
+};
+
+export const Shown = Template.bind({});
+Shown.args = {
+    open: true,
+};
+
+export const Simple = Template2.bind({});
+Simple.args = {
+    open: false,
+};
