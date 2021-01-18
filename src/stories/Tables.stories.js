03421b8fec87befe2bc33ca677c0da5a939573c5
Thoompje123 <unconfigured@null.spigotmc.org>
2021-01-18 15:25:29 +0100

    added + converted Tables component to Tailwind

---
+++ src/stories/Tables.stories.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import '../styles/style.css';
+import * as Tables from '../components/Tables/Tables'
+
+export default {
+    title: 'Example/Tables',
+    component: Tables.Table,
+    argsTypes: {}
+};
+
+const Template = (args) => <Tables.Table {...args}>
+    <thead>
+    <tr>
+        <th>First name</th>
+        <th>Last name</th>
+        <th>Role</th>
+        <th>Email</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>Adam</td>
+        <td>Zapel</td>
+        <td>Admin</td>
+        <td>azapel@mail.com</td>
+    </tr>
+    <tr>
+        <td>Adam</td>
+        <td>Zapel</td>
+        <td>Admin</td>
+        <td>azapel@mail.com</td>
+    </tr>
+    <tr>
+        <td>Adam</td>
+        <td>Zapel</td>
+        <td>Admin</td>
+        <td>azapel@mail.com</td>
+    </tr>
+    <tr>
+        <td>Adam</td>
+        <td>Zapel</td>
+        <td>Admin</td>
+        <td>azapel@mail.com</td>
+    </tr>
+    </tbody>
+</Tables.Table>;
+
+export const Simple = Template.bind({});
+Simple.args = {
+    type: 'simple'
+};
+
+export const Simple_Striped = Template.bind({});
+Simple_Striped.args = {
+    type: 'striped'
+};
