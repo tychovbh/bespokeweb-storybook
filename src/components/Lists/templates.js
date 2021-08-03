aaa82e95ed93fa3782714e4e0d9aeaaaf42c7500
Tycho <info@bespokeweb.nl>
2021-08-03 09:39:18 +0200

    fix

--- src/components/Lists/templates.js
+++ src/components/Lists/templates.js
@@ -6,7 +6,7 @@ export const templates = {
     default: ({value}) => <>{value ?? ''}</>,
     json: ({value, setModal}) => <Lists.ColumnJson value={value} setModal={setModal}/>,
     jsonb: ({value, setModal}) => <Lists.ColumnJson value={value} setModal={setModal}/>,
-    json_raw: ({value}) => <ReactJson src={value}/>
+    json_raw: ({value}) => <ReactJson src={value}/>,
     tinyint: ({value}) => <Lists.ColumnBool value={value}/>,
     image: ({value, name}) => <img src={value} alt={name}/>
 }
